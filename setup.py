50057a3e615e0cf2e9e77e0af040ad800ab1fdf6
Jonas Bernardino <20jonasojonas06@gmail.com>
2025-10-30 11:29:05 -0300

    adding build.yml para github actions

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from cx_Freeze import setup, Executable # type: ignore
+import sys
 
-# Dependencies are automatically detected, but it might need
-# fine tuning.
+# Dependências extras
 build_options = {'packages': ["pygame"], 'excludes': []}
 
-base = 'gui'
+# Base correta para Windows GUI
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
 
 executables = [
-    Executable('main.py', base=base, target_name = 'snake')
+    Executable('main.py', base=base, target_name='snake')
 ]
 
-setup(name='Snake',
-      version = '1',
-      description = 'Snake clone by Jonas Bernardino',
-      options = {'build_exe': build_options},
-      executables = executables)
+setup(
+    name='Snake',
+    version='1.0',
+    description='Snake clone by Jonas Bernardino',
+    options={'build_exe': build_options},
+    executables=executables)
